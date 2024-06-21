54f19240160ae5b15ec097394c6c00e1f59977d5
Pedro Clisesque <clisesque2k18@gmail.com>
2024-06-21 00:40:23 -0300

    create migrations

---
+++ database/migrations/Revenues/000003_create_revenues_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('revenues', function (Blueprint $table) {
+            $table->id();
+            $table->string('description');
+            $table->decimal('value', 10, 2);
+            $table->date('date_transaction');
+            $table->foreignId('category_id')->constrained('revenue_categories');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('revenues');
+    }
+};
